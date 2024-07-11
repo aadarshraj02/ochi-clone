7aaaea1f1df35ab28351992f4a37b8dd0a2965f0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-11 09:25:18 +0530

    added two more h1 tags and updated font size

--- ochi/ochi/src/components/LandingPage.jsx
+++ ochi/ochi/src/components/LandingPage.jsx
@@ -5,7 +5,13 @@ function LandingPage() {
     <div className='w-full h-screen bg-zinc-900 pt-1'>
         <div className='text-structure mt-40 px-20'>
             <div className="masker">
-                <h1 className='uppercase text-9xl leading-none tracking-tighter font-semibold'>We Create</h1>
+                <h1 className='uppercase text-8xl leading-none tracking-tighter font-medium'>We Create</h1>
+            </div>
+            <div className="masker">
+                <h1 className='uppercase text-8xl leading-none tracking-tighter font-medium'>Eye Opening</h1>
+            </div>
+            <div className="masker">
+                <h1 className='uppercase text-8xl leading-none tracking-tighter font-medium'>Presentations</h1>
             </div>
         </div>
       
