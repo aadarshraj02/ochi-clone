045956c9dd5b7a5680ab5edbdbaf46fc61b7c9a6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-11 10:09:08 +0530

    updated from capitalize to uppercase

--- ochi/ochi/src/components/LandingPage.jsx
+++ ochi/ochi/src/components/LandingPage.jsx
@@ -23,14 +23,14 @@ function LandingPage() {
           return (
             <p
               key={index}
-              className="text-md font-light tracking-tighter leading-none text-white"
+              className="text-md font-light tracking-tighter leading-none text-white capitalize"
             >
               {item}
             </p>
           );
         })}
         <div className="start flex items-center gap-5">
-          <div className="px-5 py-2 border-[2px] border-zinc-500 rounded-full font-light text-sm capitalize">
+          <div className="px-5 py-2 border-[2px] border-zinc-500 rounded-full font-light text-md uppercase">
             start the project
           </div>
           <div className="w-10 h-10 rounded-full  border-[1px] border-zinc-500 flex items-center justify-center">
