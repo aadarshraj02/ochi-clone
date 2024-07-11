aa652fe754698830b7bac5e1bf1451c8e5f07063
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-11 09:55:52 +0530

    formatted document

--- ochi/ochi/src/components/LandingPage.jsx
+++ ochi/ochi/src/components/LandingPage.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen bg-zinc-900 pt-1'>
-        <div className='text-structure mt-40 px-20'>
-            {["We Create", "Eye Opening","Presentations"].map((item,index)=>{
-                return <div className="masker" key={index}>
-                <h1 className='uppercase text-8xl leading-none tracking-tighter font-medium'>{item}</h1>
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="text-structure mt-40 px-20">
+        {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
+          return (
+            <div className="masker" key={index}>
+              <h1 className="uppercase text-8xl leading-none tracking-tighter font-medium">
+                {item}
+              </h1>
             </div>
-            })}
-        </div>
-        <div className="border-t-[1px] border-zinc-600 mt-28 flex justify-between items-center py-5 px-20">
-            {["For Public and Private companies","From the first pitch to IPO"].map((item,index)=>{
-             return <p key={index} className='text-md font-light tracking-tighter leading-none text-white'>{item}</p>
+          );
+        })}
+      </div>
+      <div className="border-t-[1px] border-zinc-600 mt-28 flex justify-between items-center py-5 px-20">
+        {[
+          "For Public and Private companies",
+          "From the first pitch to IPO",
+        ].map((item, index) => {
+          return (
+            <p
+              key={index}
+              className="text-md font-light tracking-tighter leading-none text-white"
+            >
+              {item}
+            </p>
+          );
         })}
         <div className="start">
-            <div className='px-5 py-2 border-[2px] border-zinc-500 rounded-full font-light text-sm capitalize'>start the project</div>
+          <div className="px-5 py-2 border-[2px] border-zinc-500 rounded-full font-light text-sm capitalize">
+            start the project
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
