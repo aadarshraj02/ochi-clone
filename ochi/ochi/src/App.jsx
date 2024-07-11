3a466d0123c8066600d0536d39f3383a545b6e26
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-11 11:25:55 +0530

    created marquee.jsx

--- ochi/ochi/src/App.jsx
+++ ochi/ochi/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
-import Marquee from './components/Marquee'
 
 function App() {
   return (
     <div className='w-full h-screen text-white'>
      <Navbar></Navbar>
      <LandingPage></LandingPage>
-     <Marquee></Marquee>
     </div>
   )
 }
