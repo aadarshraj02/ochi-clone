58e2782cdc468444aaee8bb854dabffabbb67eb6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-11 08:56:01 +0530

    added a h1 and applied classes

--- ochi/ochi/src/components/LandingPage.jsx
+++ ochi/ochi/src/components/LandingPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className='text-structure mt-40 px-20'>
+            <div className="masker">
+                <h1 className='uppercase text-9xl leading-none tracking-tighter font-semibold'>We Create</h1>
+            </div>
+        </div>
       
     </div>
   )
