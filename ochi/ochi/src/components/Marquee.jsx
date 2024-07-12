a1e8297f22aa2dfd729098e818f720c69ccd20ae
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 08:16:44 +0530

    added rounded corders in top left and right

--- ochi/ochi/src/components/Marquee.jsx
+++ ochi/ochi/src/components/Marquee.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Marquee() {
   return (
-    <div className='w-full py-20 bg-[#004D43] rounded-2xl' >
+    <div className='w-full py-20 bg-[#004D43] rounded-tl-2xl rounded-tr-2xl' >
       <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap gap-10 overflow-hidden px-5 py-10">
         <h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</h1>
         <h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</h1>
