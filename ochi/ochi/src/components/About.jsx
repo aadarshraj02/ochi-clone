0033bdd83dfffd7732adb6907db45e99114cae75
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 08:50:33 +0530

    added h1

--- ochi/ochi/src/components/About.jsx
+++ ochi/ochi/src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full py-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl text-black'>
-      <h1>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+    <div className='w-full p-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl text-black'>
+      <h1 className='text-[4vw] leading-[4.5vw] tracking-tight'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
     </div>
   )
 }
