88b1a649ef41f951e342b338de6aa8ed43f2376d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 09:42:13 +0530

    added a border

--- ochi/ochi/src/components/About.jsx
+++ ochi/ochi/src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className='w-full p-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl text-black'>
       <h1 className='text-[4vw] leading-[4.5vw] tracking-tight'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
-      <div className="w-full"></div>
+      <div className="w-full border-[1px] border-[#82914f] mt-20"></div>
     </div>
   )
 }
