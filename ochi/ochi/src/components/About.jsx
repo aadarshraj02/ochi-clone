99aa43c7a5053173f1070014718bd477b930ee70
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 08:36:50 +0530

    changed text color to black

--- ochi/ochi/src/components/About.jsx
+++ ochi/ochi/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full py-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl'>
+    <div className='w-full py-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl text-black'>
       <h1>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
     </div>
   )
