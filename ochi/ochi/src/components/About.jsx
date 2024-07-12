87d45ec4ef98dbac839b214e364efc94e7570cb3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 09:52:09 +0530

    added width and a button with text h1

--- ochi/ochi/src/components/About.jsx
+++ ochi/ochi/src/components/About.jsx
@@ -8,7 +8,14 @@ function About() {
         to raise funds, sell products, explain complex ideas, and hire great
         people.
       </h1>
-      <div className="w-full border-[1px] border-[#82914f] mt-20"></div>
+      <div className="w-full border-t-[1px] border-[#82914f] mt-20 pt-10">
+        <div className="w-1/2">
+          <h1 className="text-[3vw] tracking-tight">Our Approach</h1>
+          <button className="px-7 py-3 bg-zinc-900 rounded-full text-white mt-10 flex  gap-5 items-center">Read More
+            <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
