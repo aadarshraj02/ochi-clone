275407d25d955d0306046ad4d0620f08776183d7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 08:51:47 +0530

    added a div

--- ochi/ochi/src/components/About.jsx
+++ ochi/ochi/src/components/About.jsx
@@ -4,6 +4,7 @@ function About() {
   return (
     <div className='w-full p-20 bg-[#cdea68] rounded-tl-2xl rounded-tr-2xl text-black'>
       <h1 className='text-[4vw] leading-[4.5vw] tracking-tight'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+      <div className="w-full"></div>
     </div>
   )
 }
