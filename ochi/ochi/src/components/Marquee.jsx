9b8d7afaba20f86fad965d66c664d9753f7ede0a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-12 08:21:54 +0530

    converted h1 to motion.h1

--- ochi/ochi/src/components/Marquee.jsx
+++ ochi/ochi/src/components/Marquee.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Marquee() {
+
+  
   return (
     <div className='w-full py-20 bg-[#004D43] rounded-tl-2xl rounded-tr-2xl' >
       <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap gap-10 overflow-hidden px-5 py-10">
-        <h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</h1>
-        <h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</h1>
-        <h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</h1>
+        <motion.h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
+        <motion.h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
+        <motion.h1 className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
       </div>
     </div>
   )
