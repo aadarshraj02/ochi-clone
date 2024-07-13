1cab6a104eb0d329b2ae4ff01613d6c0e3a5e32b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 22:32:51 +0530

    addedbutton

--- ochi/ochi/src/components/Cards.jsx
+++ ochi/ochi/src/components/Cards.jsx
@@ -4,11 +4,14 @@ function Cards() {
   return (
     <div className="w-full h-screen bg-zinc-100 flex gap-5 items-center px-32">
       <div className="card-container w-1/2 h-[50vh]">
-        <div className="card w-full h-full rounded-xl bg-[#004d43] flex items-center justify-center">
+        <div className="card w-full h-full rounded-xl bg-[#004d43] flex items-center justify-center relative">
           <img
             src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"
             alt=""
           />
+          <button className="absolute left-10 bottom-10 px-6 py-1 border-[1px] rounded-full">
+            &copy;2019-2022
+          </button>
         </div>
       </div>
       <div className="card-container w-1/2 h-[50vh] flex gap-5">
