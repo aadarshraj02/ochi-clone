756d6776369741d0de73fdee76dab9695ab36e20
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 22:26:15 +0530

    added card conatiner

--- ochi/ochi/src/components/Cards.jsx
+++ ochi/ochi/src/components/Cards.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Cards() {
   return (
-    <div>
-      
+    <div className="w-full h-screen bg-zinc-100 flex gap-5 items-center px-32">
+      <div className="card-container w-1/2 h-[50vh]">
+        <div className="card w-full h-full rounded-xl bg-[#004d43]"></div>
+      </div>
+      <div className="card-container w-1/2 h-[50vh] flex gap-5">
+        <div className="card w-1/2 h-full rounded-xl  bg-[#004d43]"></div>
+        <div className="card w-1/2 h-full rounded-xl bg-[#004d43]"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
