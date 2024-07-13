eea7f372796309e1fd9f5522e1c3101c2d0beec6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 10:50:11 +0530

    added h1

--- ochi/ochi/src/components/Projects.jsx
+++ ochi/ochi/src/components/Projects.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Projects() {
-  return <div></div>;
+  return (
+    <div className="w-full py-10">
+      <div className="w-full px-10">
+        <h1>Featured</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
