6e996c1a0b7f0c196b91586b2ec9eb1a849447a3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 09:11:06 +0530

    added two circles

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -4,7 +4,12 @@ function Eyes() {
   return (
     <div className="eyes  w-full h-screen">
       <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center relative'>
-        <div className="absolute w-1/2  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-700"></div>
+        <div className="absolute w-1/3  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between">
+            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+                
+            </div>
+            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100"></div>
+        </div>
       </div>
     </div>
   );
