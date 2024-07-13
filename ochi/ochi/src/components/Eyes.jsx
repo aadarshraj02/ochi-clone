7a3a9fe8579f00c1a04ba6201125f95ae6dad243
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 09:13:59 +0530

    added two centered cicle

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -5,10 +5,12 @@ function Eyes() {
     <div className="eyes  w-full h-screen">
       <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center relative'>
         <div className="absolute w-1/3  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between">
-            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100">
-                
+            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
+                <div className="w-2/3 h-2/3 rounded-full bg-zinc-900"></div>
             </div>
-            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100"></div>
+            <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
+            <div className="w-2/3 h-2/3 rounded-full bg-zinc-900"></div>
+            </div>
         </div>
       </div>
     </div>
