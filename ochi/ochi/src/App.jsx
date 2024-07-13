42fc72ac4a61f528aaf90fc415d66836838caeb2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 10:47:44 +0530

    imported projects

--- ochi/ochi/src/App.jsx
+++ ochi/ochi/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import LandingPage from './components/LandingPage'
-import Marquee from './components/Marquee'
-import About from './components/About'
-import Eyes from './components/Eyes'
+import React from "react";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
+import About from "./components/About";
+import Eyes from "./components/Eyes";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className='w-full min-h-screen text-white bg-zinc-900'>
-     <Navbar></Navbar>
-     <LandingPage></LandingPage>
-     <Marquee></Marquee>
-     <About></About>
-     <Eyes></Eyes>
+    <div className="w-full min-h-screen text-white bg-zinc-900">
+      <Navbar></Navbar>
+      <LandingPage></LandingPage>
+      <Marquee></Marquee>
+      <About></About>
+      <Eyes></Eyes>
+      <Projects></Projects>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
