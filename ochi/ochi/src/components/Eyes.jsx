5fa5c6e3c8be0ef1d7a19ced629f840dce04fc1a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 08:57:06 +0530

    added centered div

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Eyes() {
   return (
     <div className='eyes  w-full h-screen'>
-        <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center '></div>
+        <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center relative'>
+        <div className='absolute w-1/2 h-96 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-700'></div>
+        </div>
     </div>
   )
 }
