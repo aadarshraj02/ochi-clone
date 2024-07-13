ab8b52ab5a3614247f0ee6255c3d0f52a413fd25
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 08:51:27 +0530

    added background image

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Eyes() {
   return (
-    <div>
-      
+    <div className='eyes  w-full h-screen'>
+        <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center '></div>
     </div>
   )
 }
