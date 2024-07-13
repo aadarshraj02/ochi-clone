873f49197bc4e2d9150056a125064eefee00bfce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 09:38:10 +0530

    formatted the document

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -7,17 +7,16 @@ function Eyes() {
         <div className="absolute w-1/3  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between">
           <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
             <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 flex items-center justify-center">
-                <div className="line w-full h-10 ">
+              <div className="line w-full h-10 ">
                 <div className="w-8 h-8 rounded-full bg-zinc-100"></div>
-                </div>
+              </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
             <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 flex items-center justify-center">
-                <div className="line w-full h-10">
+              <div className="line w-full h-10">
                 <div className="w-8 h-8 rounded-full bg-zinc-100"></div>
-                </div>
-            
+              </div>
             </div>
           </div>
         </div>
