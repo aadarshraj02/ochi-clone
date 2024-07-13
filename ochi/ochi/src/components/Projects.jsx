a92c9defa1a83b65d6cad0661149c9dbc1aff320
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-13 11:48:22 +0530

    added a h1 and div cards

--- ochi/ochi/src/components/Projects.jsx
+++ ochi/ochi/src/components/Projects.jsx
@@ -3,8 +3,11 @@ import React from "react";
 function Projects() {
   return (
     <div className="w-full py-10">
-      <div className="w-full px-10">
-        <h1>Featured</h1>
+      <div className="w-full px-20  border-b-[1px] border-zinc-500 pb-10">
+        <h1 className="text-8xl tracking-tight">Featured Projects</h1>
+        <div className="cards">
+          <div className="card"></div>
+        </div>
       </div>
     </div>
   );
