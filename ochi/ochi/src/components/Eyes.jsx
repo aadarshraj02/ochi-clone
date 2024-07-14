a7ffe94d8069406f54c059b4ea9631ce28a6baed
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-14 19:57:32 +0530

    added scroll speed on eyes div

--- ochi/ochi/src/components/Eyes.jsx
+++ ochi/ochi/src/components/Eyes.jsx
@@ -20,8 +20,12 @@ function Eyes() {
   }, []);
 
   return (
-    <div className="eyes  w-full h-screen">
-      <div className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center relative'>
+    <div className="eyes  w-full h-screen overflow-hidden">
+      <div
+        data-scroll
+        data-scroll-speed="-.6"
+        className='w-full h-full bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center relative'
+      >
         <div className="absolute w-1/3  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between">
           <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center relative">
             <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 relative flex items-center justify-center ">
