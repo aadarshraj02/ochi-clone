707bcd0eccab1da78c2d67e40268cbd2bcfa38d0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-14 13:19:15 +0530

    added two divs

--- ochi/ochi/src/components/Footer.jsx
+++ ochi/ochi/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div className="w-full h-screen bg-zinc-900"></div>;
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <div className="w-1/2"></div>
+      <div className="w-1/2"></div>
+    </div>
+  );
 }
 
 export default Footer;
