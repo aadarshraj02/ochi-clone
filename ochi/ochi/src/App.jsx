f8f92fc98ac6109f51beebd3a2f41220cf2964a9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-14 13:58:24 +0530

    added locomotive scroll

--- ochi/ochi/src/App.jsx
+++ ochi/ochi/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
@@ -7,8 +8,10 @@ import Eyes from "./components/Eyes";
 import Projects from "./components/Projects";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar></Navbar>
