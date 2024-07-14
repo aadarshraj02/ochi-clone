04d6009d1203e8f017cd23ac066a42a253bf5c6a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-14 20:40:53 +0530

    fixed error of hovering

--- ochi/ochi/src/components/Projects.jsx
+++ ochi/ochi/src/components/Projects.jsx
@@ -2,7 +2,15 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 function Projects() {
-  const [isHovering, setHovering] = useState(false);
+  const [isHovering, setHovering] = useState({ fyde: false, vise: false });
+
+  const handleMouseEnter = (card) => {
+    setHovering((prevState) => ({ ...prevState, [card]: true }));
+  };
+
+  const handleMouseLeave = (card) => {
+    setHovering((prevState) => ({ ...prevState, [card]: false }));
+  };
 
   return (
     <div className="w-full py-10">
@@ -12,8 +20,8 @@ function Projects() {
       <div className="px-20">
         <div className="cards w-full flex gap-10 mt-10">
           <div
-            onMouseEnter={() => setHovering(true)}
-            onMouseLeave={() => setHovering(false)}
+            onMouseEnter={() => handleMouseEnter("fyde")}
+            onMouseLeave={() => handleMouseLeave("fyde")}
             className="card-container w-1/2  h-[80vh] relative"
           >
             <h1 className="absolute flex z-10 text-7xl tracking-tight left-full -translate-x-1/2 top-1/2 -translate-y-1/2 overflow-hidden">
@@ -37,12 +45,22 @@ function Projects() {
               />
             </div>
           </div>
-          <div className="card-container w-1/2  h-[80vh] relative">
+          <div
+          onMouseEnter={() => handleMouseEnter("vise")}
+          onMouseLeave={() => handleMouseLeave("vise")}
+            className="card-container w-1/2  h-[80vh] relative flex overflow-hidden"
+          >
             <h1 className="absolute z-10 text-7xl tracking-tight right-full translate-x-1/2 top-1/2 -translate-y-1/2">
               {"VISE".split("").map((item, index) => (
-                <span className="" key={index}>
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={{ y: isHovering ? "0" : "100%" }}
+                  transition={{ ease: [0.45, 0, 0.55, 1], duration: 0.5 }}
+                  className="inline-block"
+                  key={index}
+                >
                   {item}
-                </span>
+                </motion.span>
               ))}
             </h1>
             <div className="card w-full h-full rounded-xl">
