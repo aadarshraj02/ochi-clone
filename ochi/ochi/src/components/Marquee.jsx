63974f03a4178ecfaa3c5cf83d92b9e454bebaa9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-14 19:36:54 +0530

    formatted code increased duration

--- ochi/ochi/src/components/Marquee.jsx
+++ ochi/ochi/src/components/Marquee.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 function Marquee() {
-
-  
   return (
-    <div className='w-full py-20 bg-[#004D43] rounded-tl-2xl rounded-tr-2xl' >
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap gap-10 overflow-hidden px-5 py-10">
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity, duration:5}} className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity, duration:5}} className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
-        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity, duration:5}} className='text-[13vw] leading-none font-bold'>WE ARE OCHI</motion.h1>
+    <div className="w-full py-20 bg-[#004D43] rounded-tl-2xl rounded-tr-2xl">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden px-5 py-10 pr-5">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[13vw] leading-none font-bold pr-5"
+        >
+          WE ARE OCHI
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[13vw] leading-none font-bold pr-5"
+        >
+          WE ARE OCHI
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[13vw] leading-none font-bold pr-5"
+        >
+          WE ARE OCHI
+        </motion.h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default Marquee
+export default Marquee;
